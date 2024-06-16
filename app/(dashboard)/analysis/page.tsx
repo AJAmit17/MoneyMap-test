5dd660f4a374d0690958f459bd21a11deebcfeee
AJAmit17 <amitjagadeesh2004@gmail.com>
2024-06-16 13:15:18 +0530

    Fix: Streamlit embedding

--- app/(dashboard)/analysis/page.tsx
+++ app/(dashboard)/analysis/page.tsx
@@ -1,17 +1,22 @@
+import Link from "next/link";
 import React from "react";
 
 const Analysis = () => {
   return (
-    <div>
-      <h1 className="text-2xl font-bold mb-4">
-        Data Analysis
-      </h1>
-      <div className="relative w-full pb-[75%] overflow-hidden">
-        <iframe
-          title="MoneyMap Analysis App"
-          src="https://moneymap.streamlit.app/?embed_options=show_colored_line,light_theme,show_toolbar,show_padding,show_footer,dark_theme"
-          className="absolute top-0 left-0 w-full h-full border-0"
-        ></iframe>
+    <div className="max-w-screen-lg mx-auto p-4 min-h-full">
+      <div className="rounded-lg shadow-lg overflow-hidden">
+        <h1 className="text-3xl font-bold text-center p-4">
+          Finance Analysis
+          <p className=" text-gray-600 text-sm">NOTE: Please Export your Transaction csv file from <Link className="text-blue-700" href={`/transactions`}>Transaction</Link> page and Upload it here</p>
+        </h1>
+        <div className="relative pb-[100%]">
+          <iframe
+            title="MoneyMap Analysis App"
+            src="https://moneymap.streamlit.app/?embed=true"
+            className="absolute top-0 left-0 w-full h-full border-0 rounded-lg"
+            style={{ borderRadius: "inherit" }}
+          ></iframe>
+        </div>
       </div>
     </div>
   );
