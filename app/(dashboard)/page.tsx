477c7d28e4fdf010860b08d70fe35cf5e601baa9
Amit Acharya <amitjagadeesh2004@gmail.com>
2024-06-23 01:03:11 +0530

    Update page.tsx

--- app/(dashboard)/page.tsx
+++ app/(dashboard)/page.tsx
@@ -4,7 +4,6 @@ import Image from "next/image";
 import { ContainerScroll } from "@/components/ipadScrollAnimate";
 import HeroSectionSimpleCentred from "@/components/hero";
 import { Button } from "@/components/ui/button";
-import { VelocityScroll } from "@/components/scroll-velocity";
 
 export default function HeroScrollDemo() {
   return (
@@ -13,8 +12,8 @@ export default function HeroScrollDemo() {
       <div className="flex items-center justify-center flex-col pb-12 px-12">
         <Image
           src="/hero.png"
-          width={1200} // Further increased width
-          height={1200} // Further increased height
+          width={1200}
+          height={1200}
           alt="Logo"
           className="max-w-full object-contain"
         />
